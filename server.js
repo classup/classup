013cdd7281173db028ab806eadcd172b2742655e
Rushabh <rusamgr8@gmail.com>
2016-03-09 19:39:59 +0530

    server.js editted

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + ‘/app’));
-app.listen(port);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
